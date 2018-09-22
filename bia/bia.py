41f22df4f818cb5d8169e5a5a6b8478ffba34f91
gavindsouza <gavin18d@gmail.com>
2018-09-22 17:17:58 +0530

    [ADDED] main function for app [CHANGES] bia: path fixes and format changes [CHANGES] browser: refactored

--- bia/bia.py
+++ bia/bia.py
@@ -7,8 +7,8 @@ intended flow:
 + point 2 consists of everything in this file
 
 if current english db is unsatisfactory 
-	try: https://wordnet.princeton.edu/
-	ref: http://androidtech.com/html/wordnet-mysql-20.php
+    try: https://wordnet.princeton.edu/
+    ref: http://androidtech.com/html/wordnet-mysql-20.php
 
 advanced applications (as per plan):
 https://www.topbots.com/4-different-approaches-natural-language-processing-understanding/	
@@ -18,59 +18,59 @@ from itertools import combinations_with_replacement, combinations
 import json
 
 # imports - module imports
-from .browser import *
-
+from bia.browser import *
 
 
 def generate_keys(suggested):
-	"""
-	suggested is type(list)
-	pass_list is type(list)
-	Inputs suggested keywords to generate likely list of passwords
-	"""
-	pass_list = []
-	
-	for r in range(11,12):
-		pass_list = [''.join(x) for x in combinations(''.join(suggested), r)]
+    """
+    suggested is type(list)
+    pass_list is type(list)
+    Inputs suggested keywords to generate likely list of passwords
+    """
+    pass_list = []
 
-	return pass_list
+    for r in range(11, 12):
+        pass_list = [''.join(x) for x in combinations(''.join(suggested), r)]
+
+    return pass_list
 
 
 def import_dictionary():
-	"""
-	will be called by generate_keys function: helps search meaningful words
-	"""
+    """
+    will be called by generate_keys function: helps search meaningful words
+    """
     dictionary = []
     try:
-        file = open(r"C:\Users\Gavin\Desktop\data\words_dictionary.json") #location of your scrambled word file
+        file = open(r"C:\Users\Gavin\Desktop\data\words_dictionary.json")  # location of your scrambled word file
         fileContents = list(json.load(file).keys())
-	finally:
+    finally:
         file.close()
     for i in range(len(fileContents)):
-        dictionary.extend(fileContents[i].split()) #changes the list by removing \n's from line breaks in text file
+        dictionary.extend(fileContents[i].split())  # changes the list by removing \n's from line breaks in text file
     return dictionary
 
 
 def import_scrambled_words():
-	"""
-	will be called by the generate_keys function: to check for words
-	"""
+    """
+    will be called by the generate_keys function: to check for words
+    """
     scrambledWords = []
     try:
-        file = """here take input of permutation of select words""" #location of your scrambled word file
+        file = """here take input of permutation of select words"""  # location of your scrambled word file
         fileContents = list(json.load(file).keys())
-        fileContents = file.readlines() #read text file and store each new line as a string
+        fileContents = file.readlines()  # read text file and store each new line as a string
     finally:
         file.close()
     for i in range(len(fileContents)):
-        scrambledWords.extend(fileContents[i].split()) #changes the list by removing \n's from line breaks in text file
+        scrambledWords.extend(
+            fileContents[i].split())  # changes the list by removing \n's from line breaks in text file
     return scrambledWords
 
 
 def unscramble_word(scrambledWord):
-	"""
-	will be called by generate_keys function: searches meaningful words
-	"""
+    """
+    will be called by generate_keys function: searches meaningful words
+    """
     countToMatch = len(scrambledWord)
     matchedWords = []
     string = ""
@@ -87,7 +87,7 @@ def unscramble_word(scrambledWord):
         if len(matchedWord) == len(scrambledWord):
             print(matchedWord)
             string = matchedWord
-            break #this returns only one unscrambles word
+            break  # this returns only one unscrambles word
     return string
 
 
@@ -99,7 +99,7 @@ if __name__ == '__main__':
         dictionary = import_dictionary()
         for x in scrambled:
             finalString += unscramble_word(x)
-            finalString +=", "
+            finalString += ", "
         len(finalString)
 
         print(finalString)
--- bia/browser.py
+++ bia/browser.py
@@ -13,40 +13,41 @@ from selenium.webdriver.support import expected_conditions as EC
 # 2. link checker returns 0/1 bot
 
 def formBot():
-	# to be auto fill bot
-	pass
+    # to be auto fill bot
+    pass
 
 
 def smartBot():
-	# checks only 1 or 0
-	pass
+    # checks only 1 or 0
+    pass
 
 
 def url(site_url, webdriver_path):
-	"""
-	webdriver testing function
-	opens the browser and site
-	"""
-	url = site_url
-	browser = webdriver.Chrome(webdriver_path)
-	browser.get(url)
+    """
+    webdriver testing function
+    opens the browser and site
+    """
+    url = site_url
+    browser = webdriver.Chrome(webdriver_path)
+    browser.get(url)
 
-	browser.close()
+    browser.close()
 
 
 def __get_path_of_chrome_driver():
-	# (finds)? and returns path of webdriver 
-	# unnecessary: first checks a log file if path(s) exist else searches the whole drive for it
-	chrome_path = os.abspath(r'\webdriver\chromedriver.exe')
-	
-	return chrome_path
+    # (finds)? and returns path of webdriver
+    # unnecessary: first checks a log file if path(s) exist else searches the whole drive for it
+    chrome_path = os.path.abspath(r'\webdriver\chromedriver.exe')
+
+    return chrome_path
 
 
 def __tree(path, *args):
-	# redundant function
-	# from image_delete_from_movies.py
-	# refactoring and linking needed
-	# modify function to return webdriver path
+    # redundant function
+    # from image_delete_from_movies.py
+    # refactoring and linking needed
+    # modify function to return webdriver path
+    app = []
     if path != '':
         path = path
     else:
@@ -54,12 +55,11 @@ def __tree(path, *args):
     try:
         for object in os.listdir(path):
             if os.path.isdir(path + '/' + object):
-                tree(path + '/' + object)
+                __tree(path + '/' + object)
             elif os.path.isfile(path + '/' + object):
                 ext = object.split('.')[-1]
-                if ext in image:
-                    img.append('{0}/{1}'.format(path, object))
+                if ext is 'exe':
+                    app.append('{0}/{1}'.format(path, object))
 
     except NotADirectoryError or FileNotFoundError or OSError:
         pass
-
